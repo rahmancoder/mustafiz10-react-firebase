7e37427aaf8d38519d4624b5856df0952ab1ea3d
Mustafizur <sayhemrahman@gmail.com>
2021-10-18 18:31:44 +0600

    header footer added

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,9 +2,40 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
+        <div className="bg-warning text-dark">
+            <div className="d-flex  mt-4 ">
+                <div className="col-md-5 mt-4 mx-3">
+                    <h5>HealthCare Online & Support</h5>
+                    <p>Online care or an e-visit is a way of obtaining medical care for problems which do not require a physical examination and which do not require treatment beyond a prescription available online or diagnostic tests beyond blood tests.</p>
+                </div>
+                <div className="mt-4">
+                    <h5>CONTACT US</h5>
+                    <p><i class="far fa-envelope-open"></i> EMAIL:onlinehealthcare@gmail.com</p>
+                    <p><i class="fas fa-mobile-alt"></i> Phone:+8801871414746</p>
+                    <p><i class="fas fa-map-marker-alt"></i> Address:Dhaka, Bangladesh</p>
+                    <p><i class="fas fa-fax"></i> Fax:www.sayhemrahman.com</p>
+                </div>
+                <div className="mt-4">
+                    <h5>INFORMATION</h5>
+                    <p>Diagonosis</p>
+                    <p>LAB Test</p>
+                    <p>Treatment Guide</p>
+                    <p>Online Doctors</p>
 
-            <h1>This is footer</h1>
+                </div>
+                <div className="mt-4">
+                    <h5>COMPANY</h5>
+                    <p>Our Doctors</p>
+                    <p>Specialize Doctor</p>
+                    <p>Medicine</p>
+                    <p>Diabates and Insuline</p>
+                    <p>Exported Medicine</p>
+                </div>
+
+            </div>
+            <hr />
+            <p>Created by © MD Mustafizur Rahman  All rights reserved</p>
+
         </div>
     );
 };
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { Container, Navbar, Button, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            <h1>This is Header: Mustafiz Online Healthcare services</h1>
+        <>
+            <Navbar sticky="top" bg="warning" variant="dark">
+                <Container>
+                    <Navbar.Brand href="/home"><i class="fas fa-doctor-code"></i> HealthCare Online & Support</Navbar.Brand>
+                    <Nav className="me-5 p-2 text-center">
+                        <Button className="bg-warning"><Link className="m-2 text-decoration-none" to="/home">Home</Link> </Button>
+                        <Link className="m-2 text-decoration-none" to="/services">Services</Link>
+                        <Link className="m-2 text-decoration-none" to="/doctors">Doctor</Link>
+                        <Link className="m-2 text-decoration-none" to="/login">LogIn</Link>
+                        <Link className="m-2 text-decoration-none" to="/register">Register</Link>
+                        <Link className="m-2 text-decoration-none" to="/questions">FAQ</Link>
+                        {/* <Link className="m-2" to="/notfound">Not Found</Link> */}
+                    </Nav>
+                </Container>
+            </Navbar>
 
-        </div>
+
+        </>
     );
 };
 
