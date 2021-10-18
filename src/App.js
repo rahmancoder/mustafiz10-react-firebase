d09304165dced92a2053aad37ffb7ee29575d54b
Mustafizur <sayhemrahman@gmail.com>
2021-10-18 14:16:16 +0600

    component setup with route and testing all routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 
 import './App.css';
+// Bootstrap Link
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import NotFound from './Components/NotFound/NotFound';
+import Home from './Components/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1> Mustafiz Healthcare site with firebase and React</h1>
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Route exact path='/'>
+            <Home></Home>
+          </Route>
+
+          <Route exact path='/home'>
+            <Home></Home>
+
+          </Route>
+
+
+
+
+          <Route path='*'>
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+
+        <Footer></Footer>
+
+      </Router>
+
     </div>
   );
 }
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+
+            <h1> This is Home Needs to render 2 more route here</h1>
+        </div>
+    );
+};
+
+export default Home;
